8baf1fdf9c0a89b37627c73752c4a773e09bbdb3
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-01-04 19:14:01 -0300

    feat: removendo linha com falha de dado por api

--- src/processamento/limpar_dados.py
+++ src/processamento/limpar_dados.py
@@ -10,14 +10,13 @@ def processar_dados(data: dict) -> pd.DataFrame:
     df = df[[
         "dataApuracao",
         "dezenas_sorteadas",
-        "valorArrecadado",
         "numero",
     ]]
 
     df = df.rename(columns={
         "dataApuracao": "data_apuracao",
-        "valorArrecadado": "valor_arrecadado",
         "numero": "numero_concurso",
     })
 
     return df
+
